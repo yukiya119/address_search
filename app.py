63dd3ee0498572c654c17c60984e66b8cfb6e0e9
yukiya119 <yukinosuke911@gmail.com>
2022-01-29 15:42:36 +0900

    main関数にまとめた
    ついていくのがやっと…

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-import requests
+def main():
+    import requests
 
-zipcode = "0294102"
-# zipcode = input("郵便番号〈ハイフン無し〉は？ >> ")
+    zipcode = "0294102"
+    # zipcode = input("郵便番号〈ハイフン無し〉は？ >> ")
 
-url = f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
-# url = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=" +zipcode 上行と同義
+    url = f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
+    # url = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=" +zipcode 上行と同義
 
-response = requests.get(url)
+    response = requests.get(url)
 
-results = response.json()['results']
+    results = response.json()['results']
 
-pref_name = results[0]['address1']
-city_name = results[0]['address2']
-town_name = results[0]['address3']
+    pref_name = results[0]['address1']
+    city_name = results[0]['address2']
+    town_name = results[0]['address3']
 
-address = f'{pref_name}{city_name}{town_name}'
+    address = f'{pref_name}{city_name}{town_name}'
+    print(address)
 
-print(address)
+
+if __name__ == '__main__':
+    main()
