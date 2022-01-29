047982132782dc12bca954e7a8ba5f94ed8d97cf
yukiya119 <yukinosuke911@gmail.com>
2022-01-29 16:13:34 +0900

    関数化して整理
    ついていくのがやっと…

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
-def main():
-    import requests
+import requests
 
-    zipcode = "0294102"
-    # zipcode = input("郵便番号〈ハイフン無し〉は？ >> ")
 
+def search_address(zipcode):
     url = f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
     # url = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=" +zipcode 上行と同義
-
     response = requests.get(url)
-
     results = response.json()['results']
-
     pref_name = results[0]['address1']
     city_name = results[0]['address2']
     town_name = results[0]['address3']
-
     address = f'{pref_name}{city_name}{town_name}'
+    return address
+
+
+def main():
+    zipcode = "0294102"
+
+    # zipcode = input("郵便番号〈ハイフン無し〉は？ >> ")
+    address = search_address(zipcode=zipcode)
+
     print(address)
 
 
