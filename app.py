11a5f34293ae2afbae43c1136520057d4c532ef4
yukiya119 <yukinosuke911@gmail.com>
2022-01-29 15:00:27 +0900

    inputに対応
    通信成功(200)

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import requests
 
-url = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=0294102"
+zipcode = "0294102"
+# zipcode = input("郵便番号〈ハイフン無し〉は？ >> ")
+
+url = f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
+# url = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=" +zipcode 上行と同義
 
 response = requests.get(url)
 print(response)
